ef2cd05e5f7f9ade3efd35e40e052986018ae979
sarapinit <sarapinit@ya.ru>
2016-09-16 21:07:30 +0700

    Add project files.

---
+++ ConsoleApplication72/BotState.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApplication72
+{
+
+
+  public abstract class BotState
+  {
+    private static readonly Dictionary<string, BotState> States = new Dictionary<string, BotState>
+    {
+      [nameof(OnState)] = new OnState(),
+      [nameof(OffState)] = new OffState()
+    };
+ 
+    public static BotState State(string name)
+    {
+        BotState state;
+        if(States.TryGetValue(name, out state))
+        {
+          return state;
+        }
+        throw new ArgumentException($"Не найдено состояние {state}");
+    }
+
+    public void Handle(BotContext context)
+    {
+      Action<BotContext> action;
+      if(Actions.TryGetValue(context.CurrentCommand, out action))
+      {
+        action.Invoke(context);
+      }
+      else
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Неверная команда!");
+
+        Console.WriteLine("Доступные команды:");
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        foreach (var key in Actions.Keys)
+        {
+          Console.WriteLine(key);
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+      }
+    }
+    protected abstract Dictionary<string, Action<BotContext>> Actions { get; }
+  }
+
+  public class OnState : BotState
+  {
+    private Dictionary<string, Action<BotContext>> _actions = new Dictionary<string, Action<BotContext>>
+    {
+      ["/on"] = (context) => { Console.WriteLine("state is already on!"); }, 
+      ["/off"] = (context) => { Console.WriteLine("state switched to off"); context.State = State(nameof(OffState)); },
+      ["/echo"] = (context) => { Console.WriteLine(context.CurrentMessage); }
+    };
+    protected override Dictionary<string, Action<BotContext>> Actions => _actions;
+    
+  }
+
+  public class OffState : BotState
+  {
+    private Dictionary<string, Action<BotContext>> _actions = new Dictionary<string, Action<BotContext>>
+    {
+      ["/off"] = (context) => { Console.WriteLine("state is already off!"); },
+      ["/on"] = (context) => { Console.WriteLine("state switched to on"); context.State = State(nameof(OnState)); }
+    };
+    protected override Dictionary<string, Action<BotContext>> Actions => _actions;
+  }
+}
---
+++ ConsoleApplication72/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication72
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var context = new BotContext(new OffState());
+      while (true)
+      {
+        Console.Write("user>");
+        var text = Console.ReadLine();
+        context.Request(text);
+      }
+    }
+  }
+
+  public class BotContext
+  {
+    public string CurrentCommand { get; private set; } = string.Empty;
+    public string CurrentMessage { get; private set; } = string.Empty;
+
+    public BotState State { get; set; }
+    public BotContext(BotState state)
+    {
+      State = state;
+    }
+    public void Request(string text)
+    {
+      if(text[0] == '/')
+      {
+        var index = text.IndexOf(' ');
+        if(index >= 0)
+        {
+          CurrentCommand = text.Substring(0, index);
+          CurrentMessage = text.Substring(index + 1);
+        }
+        else
+        {
+          CurrentCommand = text;
+        }
+      }
+      
+      State.Handle(this);
+    }
+  }
+}
